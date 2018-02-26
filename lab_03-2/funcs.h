61731176230b750299911038305a37dcaad61bc5
Thomas Westfall <thomas.westfall64@myhunter.cuny.edu>
2018-02-26 07:38:19 -0500

    add lab_03-2

---
+++ lab_03-2/funcs.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+double east_storage(string datex);
+double mineast();
+double maxeast();
+string compare(string a, string b);
+
+//naming this reverse-order gives an error, so I called it reverseorder
+string reverseorder(string a, string b);
