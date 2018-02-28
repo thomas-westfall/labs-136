81f51b8fe2892e6efd1da6a4a10223a96bf597a9
Thomas Westfall <thomas.westfall64@myhunter.cuny.edu>
2018-02-28 18:24:14 -0500

    add lab_04

---
+++ lab_04/funcs.cpp
@@ -0,0 +1,10 @@
+/*
+Author: Thomas Westfall
+Course: CSCI-133
+Instructor: Mr. Zamansky
+Assignment: Lab 4
+*/
+
+#include "funcs.h"
+#include <iostream>
+using namespace std;
---
+++ lab_04/main.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Thomas Westfall
+Course: CSCI-133
+Instructor: Mr. Zamansky
+Assignment: Lab 4
+
+*/
+
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+
+int main() {
+  return 0;
+
+}
